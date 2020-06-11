67efd83e74787a0657bdc0dba54dce0cc74157ef
OrcClaSH <orcaclash@gmail.com>
2020-06-11 12:08:13 +0900

    Added check item on page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,13 +1,22 @@
 # Добавил импорт для аргумента браузера
 from selenium.webdriver import Remote as RemoteWebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
     # Проставил класс для агрумента браузера, в IDE 
     # будут всплывать подсказки методов
-    def __init__(self, browser: RemoteWebDriver, url):
+    def __init__(self, browser: RemoteWebDriver, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     
     def open(self):
         self.browser.get(self.url)
+    
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
