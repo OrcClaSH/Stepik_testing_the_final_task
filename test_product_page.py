bf3a6a8b9056f22319c59d70414d2977a52140da
OrcClaSH <orcaclash@gmail.com>
2020-06-24 19:05:38 +0900

    Grouped two test cases into the TestLoginFromMainPage class

--- test_product_page.py
+++ test_product_page.py
@@ -57,7 +57,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-    #4.3.10
+    #4.3.10 the task
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = BasketPage(browser, link)
